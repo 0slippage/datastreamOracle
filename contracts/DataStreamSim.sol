b5b3e6c037dc963f3f81fbeeae8ecddcbdfdd734
Zero Slippage <99324956+0slippage@users.noreply.github.com>
2025-07-13 05:19:56 +0100

    Added client reports verifier; change solidity version for compilation.

--- contracts/DataStreamSim.sol
+++ contracts/DataStreamSim.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.19;
 
 //. 0.2.4 Instructions specified this line (https://github.com/smartcontractkit/chainlink-local/releases/tag/v0.2.4):
 //
