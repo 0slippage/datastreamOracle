294ff9bf346141036ad89eb0129b08496fbcd48a
Zero Slippage <99324956+0slippage@users.noreply.github.com>
2025-07-13 04:30:01 +0100

    Initial commit

---
+++ contracts/DataStreamSim.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+//. 0.2.4 Instructions specified this line (https://github.com/smartcontractkit/chainlink-local/releases/tag/v0.2.4):
+//
+import {DataStreamsLocalSimulator} from "@chainlink/local/src/data-streams/DataStreamsLocalSimulator.sol";
+
+//. 0.2.5 Instructions specified this line (https://github.com/smartcontractkit/chainlink-local/releases/tag/v0.2.5):
+//
+// import {CCIPLocalSimulator} from "https://github.com/smartcontractkit/chainlink-local/blob/v0.2.5/src/ccip/CCIPLocalSimulator.sol";
+// import {CCIPLocalSimulator} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
